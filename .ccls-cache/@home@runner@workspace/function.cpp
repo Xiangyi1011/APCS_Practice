e884090825227b74b3749cb47f43f44bbbd47ac0
Xiangy <enerty1011@gmail.com>
2026-03-04 14:42:22 +0000

    修改程式碼錯誤程式碼中少一個{符號, 導致temperature無法定義
    修改fahrenheit拼字錯誤

--- .ccls-cache/@home@runner@workspace/function.cpp
+++ .ccls-cache/@home@runner@workspace/function.cpp
@@ -11,10 +11,9 @@ void function1(){ //void 表示沒有回傳值（傳出值）
 
 //有傳出值-float--有參數則回傳
 //制定一個function
-float temperature(int value) //整數參數名稱value
+float temperature(int value) { //整數參數名稱value
 return 1.8*value+32; //回傳值（傳出值）
 }
-
 
 
 int main() {
@@ -26,7 +25,8 @@ int main() {
   float fahrenheit; //宣告變數(接收)
   cout << "請輸入攝氏溫度:";
   cin >> celsius; //輸入值
-  fahremheit = temperature(celsius); //呼叫自訂function,並傳入參數至接收的變數內
+  fahrenheit = temperature(celsius);
+  //呼叫自訂function,並傳入參數至接收的變數內
   //temperature短暫記憶體接收輸入參數
   cout << "華氏溫度為:" << fahrenheit << '\n'; //輸出
   return 0;
