beec329845e536a82681eb861203da7d10561db5
Xiangy <enerty1011@gmail.com>
2026-03-11 13:00:44 +0000

    使用for(:)

--- vector_2.cpp
+++ vector_2.cpp
@@ -10,15 +10,15 @@ int main(){
   double sum = 0.0 ; //變數, 值可改變
   const int NUMBER_OF_ENTRIES = 5; //常數, 值不可改變, 一般常數全部大寫
   vector<double> numbers(NUMBER_OF_ENTRIES); //建立一個有5個元素的vector實體, 預設值為0.0, double是浮點數, 可放小數
-  cout << "請輸入" << NUMBER_OF_ENTRIES << "個數值：" << '\n';
+  cout << "請輸入" << NUMBER_OF_ENTRIES << "個數值：\n" << '\n';
   for(int i=0; i<NUMBER_OF_ENTRIES; i++){
     cin >> numbers[i]; //輸入值, 存入vector
     sum += numbers[i]; //加總
   }
 
   cout << "此vector：";
-  for (int i=0; i<NUMBER_OF_ENTRIES; i++){
-    cout << numbers[i] << " ";
+  for (double elem:numbers){
+    cout << elem << " ";
   }
 
   cout << '\n';
